e69994987d1025b7c21ccde055bd842099bede35
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-07-10 18:06:13 +0530

    Create ServicesCarousel component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import './App.css'
 import RegisterProgress from './components/ui/RegisterProgress'
-import ServiceCard from './components/common/ServiceCard'
+import ServicesCarousel from './components/ServicesCarousel'
 
 function App() {
   return (
     <>
       {/* <RegisterProgress precentage={50}></RegisterProgress> */}
-      <div className='w-60 h-96 bg-black'>
-      <ServiceCard 
-        img={"src/assets/Images/Images/07.png"}
-        title={"photography"}
-        text={"Best photographers with experience"}
-      />
-      </div>
+      <ServicesCarousel />
     </>
   )
 }
