f0ceb7a6db180d29ee798ab9737114470084d32f
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-07-10 19:53:43 +0530

    Create ReviewCarousel component

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,12 @@
 import './App.css'
 import RegisterProgress from './components/ui/RegisterProgress'
-import ReviewCard from './components/common/ReviewCard'
+import ReviewCarousel from './components/ReviewCarousel'
 
 function App() {
   return (
     <>
       {/* <RegisterProgress precentage={50}></RegisterProgress> */}
-      <div className='flex flex-row items-center justify-center bg-yellow-500 gap-10'>
-      <div className='w-72 h-60 bg-black'>
-        <ReviewCard 
-          img={"src/assets/Images/Images/07.png"}
-          name={"Ramath Perera"}
-          time={"5h Ago"}
-          review={"This website made planning our wedding so much easier! We found the perfect photographer and florist in just a few clicks. Organizing Content was so easy"}
-        />
-        </div>
-        <div className='w-72 h-60 bg-black'>
-        <ReviewCard 
-          img={"src/assets/Images/Images/06.png"}
-          name={"Ramath Perera"}
-          time={"5h Ago"}
-          review={"This website made planning our wedding so much easier! We found the perfect photographer and florist in just a few clicks. Organizing Content was so easy"}
-        />
-        </div>
-        <div className='w-72 h-60 bg-black'>
-        <ReviewCard 
-          img={"src/assets/Images/Images/05.png"}
-          name={"Ramath Perera"}
-          time={"5h Ago"}
-          review={"This website made planning our wedding so much easier! We found the perfect photographer and florist in just a few clicks. Organizing Content was so easy"}
-        />
-        </div>
-        <div className='w-72 h-60 bg-black'>
-        <ReviewCard 
-          img={"src/assets/Images/Images/04.png"}
-          name={"Ramath Perera"}
-          time={"5h Ago"}
-          review={"This website made planning our wedding so much easier! We found the perfect photographer and florist in just a few clicks. Organizing Content was so easy"}
-        />
-        </div>
-      </div>
+      <ReviewCarousel />
     </>
   )
 }
