7e0b159391fdbcfdb0a276a306f0aee0b3095aef
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-07-17 20:06:31 +0530

    Create PackageCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
-// import AddCard from './components/common/AddCard'
-import CreatePackageForm from './components/CreatePackageForm'
+import AddCard from './components/common/AddCard'
+import PackageCard from './components/common/PackageCard'
+// import CreatePackageForm from './components/CreatePackageForm'
 
 function App() {
   return (
@@ -8,13 +9,22 @@ function App() {
       {/* <ServicesCarousel /> */}
       {/* <ReviewCarousel /> */}
       {/* <div className='w-96'> */}
-      {/* <div className="flex items-center justify-center h-60 w-52 bg-white p-2">
-        <AddCard />
-      </div> */}
-      {/* </div> */}
-      <div className='w-full h-full bg-white p-10'>
-        <CreatePackageForm />
+      <div className="flex items-center justify-center h-60 w-52 bg-white p-2">
+        <AddCard 
+          text={"Create Package"}
+        />
       </div>
+      <div className="flex items-center justify-center h-60 w-52 bg-white p-2">
+        <PackageCard 
+          img={"../src/assets/images/Images/01.png"}
+          text={"Package 01"}
+          button={"See more"}
+        />
+      </div>
+      {/* </div> */}
+      {/* <div className='w-full h-full bg-white p-10'>
+        <CreatePackageForm />
+      </div> */}
     </>
   )
 }
--- src/components/common/AddCard.jsx
+++ src/components/common/AddCard.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-function AddCard() {
+function AddCard(props) {
+
     return (        
-        <div className="w-full h-full bg-white rounded-lg shadow-md flex flex-col items-center justify-center p-4 border border-custom-primary cursor-pointer hover:bg-custom-primary hover:bg-opacity-10 hover:shadow-lg transition duration-300">
-        <div className="text-6xl text-custom-primary">
+        <div className="w-full h-full bg-white rounded-lg shadow-md flex flex-col gap-5 items-center justify-center p-4 border border-custom-primary cursor-pointer hover:bg-custom-primary hover:bg-opacity-10 hover:shadow-lg transition duration-300">
+        <div className="text-6xl text-custom-primary border-8 border-custom-primary rounded-md">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-12 h-12">
             <path fillRule="evenodd" d="M12 3a1 1 0 01.993.883L13 4v7h7a1 1 0 01.117 1.993L20 13h-7v7a1 1 0 01-1.993.117L11 20v-7H4a1 1 0 01-.117-1.993L4 11h7V4a1 1 0 011-1z" clipRule="evenodd" />
             </svg>
         </div>
-        <div className="text-yellow-600 text-sm font-medium">Create Package</div>
+        <div className="text-custom-primary text-sm font-medium">{props.text}</div>
         </div>
     );
 }
