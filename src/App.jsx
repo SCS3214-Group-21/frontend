5a78ee1abdc7949859d3922ea9276070b72f0c9f
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-07-21 04:09:36 +0530

    Create BlogCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import './App.css'
-import PublicRoute from './routes/PublicRoute'
+// import PublicRoute from './routes/PublicRoute'
 // import VendorProfilePage from './pages/VendorProfilePage'
 // import VendorIncomeGraph from './components/VendorIncomeGraph'
 // import BusinessDetailsForm from './components/BusinessDetailsForm'
-// import UpdateBlogForm from './components/UpdateBlogForm'
+import BlogCard from './components/common/BlogCard'
 
 function App() {
   return (
     <>
-      <PublicRoute />
-      {/* <UpdateBlogForm /> */}
+      {/* <PublicRoute /> */}
+      <div className="flex items-center justify-center h-[22rem] w-96 bg-white p-2">
+      <BlogCard 
+        img={"../src/assets/images/Images/01.png"}
+        text={"Love in Full Blooms - Navigating the Delicate Petals of Romance"}
+        date={"13 Nov, 2023"}
+        time={"05.00 PM"}
+        button={"Read Blog"}
+      />
+      </div>
       {/* <VendorProfilePage /> */}
       {/* <div className="bg-gray-100 w-full h-[30rem] p-10">
         <VendorIncomeGraph />
