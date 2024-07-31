ca8c3868ffd65677c63267fe62abc552246c73aa
Binuri Mindula <binuriwagarachchi@gmail.com>
2024-07-31 20:51:23 +0530

    Update review carousel and buttons

--- src/components/ui/PrimaryButton.jsx
+++ src/components/ui/PrimaryButton.jsx
@@ -6,11 +6,11 @@ function PrimaryButton(props) {
     return (
         <button onClick={() => Navigate(props.link)} className="border-0 rounded-full px-8 h-10 bg-custom-primary text-white transition-all duration-[600ms] ease-in-out font-semibold hover:bg-custom-gray hover:from-custom-gray hover:via-custom-gray hover:to-custom-gray hover:text-custom-secondary hover:border-2 hover:border-custom-secondary">
 
-//         <button 
+            {/* //         <button 
 //             onClick={()=>Navigate(props.link)}
 //             type="button" 
 //             className="text-white bg-gradient-to-br from-yellow-600 to-yellow-800 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-yellow-600 dark:focus:ring-yellow-700 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2.5 mb-2"
-//         >
+//         > */}
 
             {props.text}
         </button>
