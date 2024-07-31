1971d4fed6dfc59c79368fb011ea6768f8ed79d6
nemesis-9 <nyxn96@gmail.com>
2024-07-31 10:20:29 +0530

    minor changes

--- src/components/common/StatusCard.jsx
+++ src/components/common/StatusCard.jsx
@@ -8,7 +8,7 @@ function SectionCard(props) {
 
   return (
     <div className="z-10 p-10 w-72 sm:w-[30rem] md:w-[40rem] bg-white rounded-3xl shadow-custom-inset bg-opacity-50 backdrop-blur-sm">
-      <h1 className="text-black font-sans font-normal text-4xl sm:text-5xl">
+      <h1 className="text-black font-sans font-normal text-2xl sm:text-5xl">
         {title} <span className="text-custom-primary">{subtitle}</span>
       </h1>
       <br />
