07c8dd83ad26881a19ac5aacc9cb3914bba74ad1
Binuri Mindula <binuriwagarachchi@gmail.com>
2024-12-01 22:20:47 +0530

    Create update status for vendor

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6q3oili8iv"
+    "revision": "0.c796s7hnpv"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
